71d1b9005c4d84a32a58e72c7d7b16f170aadf28
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-04 01:38:16 +0100

    Use `fileSeparator` constant

--- src/scripts/revert-changes.ts
+++ src/scripts/revert-changes.ts
@@ -1,9 +1,9 @@
 import { readFileSync, rmSync, writeFileSync } from "fs";
-import { archivedVideoSuffix, fileExtensionSeparator, moviesFolderPath, splashesToRemove } from "../constants";
+import { archivedVideoSuffix, fileExtensionSeparator, fileSeparator, moviesFolderPath, splashesToRemove } from "../constants";
 
 export function revertChanges() {
     for (const loadingVideo of splashesToRemove) {
-        const fullFilePath = moviesFolderPath + "/" + loadingVideo;
+        const fullFilePath = moviesFolderPath + fileSeparator + loadingVideo;
 
         const fileExtensionSeparatorIndex = fullFilePath.lastIndexOf(fileExtensionSeparator);
         const archivedVideoFilePath = fullFilePath.substring(0, fileExtensionSeparatorIndex) + archivedVideoSuffix + fileExtensionSeparator + fullFilePath.substring(fileExtensionSeparatorIndex + 1);
